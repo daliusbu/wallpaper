1cfb5eb85e39a25aaa207f30c4c5cf1a43c2fb2f
daliusbu <daliusbu@gmail.com>
2018-11-13 15:42:17 +0200

    stage 2 Added Knp Paginator:

--- src/AppBundle/Controller/GalleryController.php
+++ src/AppBundle/Controller/GalleryController.php
@@ -10,18 +10,35 @@ class GalleryController extends Controller
 {
     /**
      * @Route("/gallery", name="gallery")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $image = '1200px-Summer-3106910_1920.jpg';
+
+        $images = [
+            'abstract-background-pink.jpg',
+                'abstract-black-and-white-wave.jpg',
+                'abstract-black-multi-color-wave.jpg',
+                'abstract-blue-green.jpg',
+                'abstract-blue-line-background.jpg',
+                'abstract-red-background-pattern.jpg',
+                'abstract-shards.jpeg',
+                'abstract-swirls.jpeg',
+                'landscape-summer-beach.jpg',
+            ];
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $images,
+            $request->query->getInt('puslapis', 1)/*page number*/,
+            4/*limit per page*/
+        );
+
+        dump ($pagination);
 
         return $this->render('gallery/index.html.twig', [
-            'images'=>[
-                $image,
-                $image,
-                $image,
-                $image,
-            ]
+            'images'=>$pagination,
         ]);
     }
 }
